6d4878a37a29a7ac8b2f1fb92a827a97b47dad0b
muxacarin <muxacarin@192.168.0.14>
2014-07-07 01:23:39 +0900

    1. 글자 크기 조

--- davinchi code/src/da_co/StartWindows.java
+++ davinchi code/src/da_co/StartWindows.java
@@ -15,6 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+import java.awt.Font;
 
 public class StartWindows {
 
@@ -22,7 +23,7 @@ public class StartWindows {
 	private GameWindows gamewindows;
 	private final Action gamestart = new GameStart();
 	private JComboBox gamepeople;
-	private JLabel label;
+	private JLabel label_1;
 
 	/**
 	 * Launch the application.
@@ -57,36 +58,38 @@ public class StartWindows {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{400, 400, 0};
-		gridBagLayout.rowHeights = new int[]{289, 289, 0};
+		gridBagLayout.rowHeights = new int[]{99, 188, 289, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JLabel lblNewLabel = new JLabel("\uB2E4\uBE48\uCE58 \uCF54\uB4DC");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.fill = GridBagConstraints.BOTH;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 0;
 		gbc_lblNewLabel.gridwidth = 2;
 		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
 		
-		label = new JLabel("");
-		GridBagConstraints gbc_label = new GridBagConstraints();
-		gbc_label.fill = GridBagConstraints.BOTH;
-		gbc_label.insets = new Insets(0, 0, 5, 0);
-		gbc_label.gridx = 1;
-		gbc_label.gridy = 0;
-		frame.getContentPane().add(label, gbc_label);
+		label_1 = new JLabel("\uAC8C\uC784 \uC778\uC6D0 : ");
+		label_1.setFont(new Font("Dialog", Font.BOLD, 50));
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.fill = GridBagConstraints.VERTICAL;
+		gbc_label_1.insets = new Insets(0, 0, 5, 5);
+		gbc_label_1.gridx = 0;
+		gbc_label_1.gridy = 1;
+		frame.getContentPane().add(label_1, gbc_label_1);
 		
 		gamepeople = new JComboBox();
-		gamepeople.setModel(new DefaultComboBoxModel(new String[] {"2", "3", "4"}));
+		gamepeople.setFont(new Font("Dialog", Font.BOLD, 60));
+		gamepeople.setModel(new DefaultComboBoxModel(new String[] {"         2", "         3", "         4"}));
 		gamepeople.setSelectedIndex(2);
 		GridBagConstraints gbc_gamepeople = new GridBagConstraints();
+		gbc_gamepeople.insets = new Insets(0, 0, 5, 0);
 		gbc_gamepeople.fill = GridBagConstraints.BOTH;
-		gbc_gamepeople.insets = new Insets(0, 0, 0, 5);
-		gbc_gamepeople.gridx = 0;
+		gbc_gamepeople.gridx = 1;
 		gbc_gamepeople.gridy = 1;
 		frame.getContentPane().add(gamepeople, gbc_gamepeople);
 		
@@ -97,7 +100,7 @@ public class StartWindows {
 		GridBagConstraints gbc_btnGameStart = new GridBagConstraints();
 		gbc_btnGameStart.fill = GridBagConstraints.BOTH;
 		gbc_btnGameStart.gridx = 1;
-		gbc_btnGameStart.gridy = 1;
+		gbc_btnGameStart.gridy = 2;
 		frame.getContentPane().add(btnGameStart, gbc_btnGameStart);
 	}
 
