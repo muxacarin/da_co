efb6e2a149928c6bcf82040e6bea06f233ebca83
muxacarin <muxacarin@192.168.0.14>
2014-07-07 01:22:51 +0900

    1. 사망판정 넣어야함.

--- davinchi code/src/da_co/DavinchiCodePlayer.java
+++ davinchi code/src/da_co/DavinchiCodePlayer.java
@@ -6,10 +6,24 @@ public class DavinchiCodePlayer {
 	
 	private boolean hand_white[] = new boolean[12];
 	private boolean hand_black[] = new boolean[12];
+	private boolean open_white[] = new boolean[12];
+	private boolean open_black[] = new boolean[12];
 	
 	DavinchiCodePlayer(){
 		Arrays.fill(hand_white,false);
 		Arrays.fill(hand_black, false);
+		Arrays.fill(open_black, false);
+		Arrays.fill(open_white, false);
+	}
+	
+	private int cardcount(Dacolor color, int number){
+		boolean[] hand = (color.equals(Dacolor.BLACK)) ? this.hand_black : this.hand_white;
+		int isthis = (hand[number]) ? 1 : 0;
+		if (color == Dacolor.BLACK )
+			if ( number == 0) return isthis;
+			else return isthis + cardcount(Dacolor.WHITE, number-1);
+		else return isthis + cardcount(Dacolor.BLACK, number);
+		
 	}
 	
 	public void cardset(Dacolor color, int number){
@@ -27,19 +41,28 @@ public class DavinchiCodePlayer {
 	}
 	
 
-	@SuppressWarnings("null")
 	public boolean cardget(Dacolor color, int number){
 		
-		switch (color){
-		case BLACK:
-			return hand_black[number];
-		case WHITE:
-			return hand_white[number];
-		
-		
-		}
-		return (Boolean) null;
+		return (color.equals(Dacolor.BLACK)) ? this.hand_black[number] : this.hand_white[number];
 	}
+	
+	public boolean opencardget(Dacolor color, int number){
+		
+		return (color.equals(Dacolor.BLACK)) ? this.open_black[number] : this.open_white[number];
+	}
+	
+	public boolean cardposition(int cardpos, Dacolor color, int number){
+		boolean[] hand = (color.equals(Dacolor.BLACK)) ? this.hand_black : this.hand_white;
+		boolean[] open = (color.equals(Dacolor.BLACK)) ? this.open_black : this.open_white;
+		
+		if (open[number]) return false;
+		
+		if (hand[number]) {
+			if(cardcount(color, number) == cardpos + 1) open[number] = true;
+		}
+		return open[number];
+	}
+	
 	
 	
 
